7ff5054a308e38c3d810836037e8bc656b37e169
gloofo <markenochmorales@hotmail.com>
2023-09-17 17:25:25 +0800

    Added invitation func

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ print(
 getEmuIndex = 0
 getPackageName = ""
 getMonoDelay = 0
+getLink = ""
+
 
 def getIndex():
     try:
@@ -44,6 +46,16 @@ def getDelay():
 
     return getMonoDelay
 
+def getMonoLink():
+    try:
+        getLink = input("Enter Invite Link: ")
+    except ValueError:
+        print("The entered value is invalid. Please try again.")
+        sys.exit()
+
+    return getLink
+
+invitationLink = getMonoLink()
 getIndex()
 getPackage()
 monoInterval = getDelay()
@@ -67,7 +79,7 @@ def start_emu(indexNumber: int):
     # set to remember window location
     memuc.set_configuration_vm("start_window_mode", "1", vm_index=indexNumber)
 
-def start_automate(indexNumber: int, p1: str, p2: str, p3: str):
+def start_automate(InviteLink: str, indexNumber: int, p1: str, p2: str, p3: str):
     #memuc.send_adb_command_vm(f"shell pm enable {packageName}", indexNumber)
     memuc.send_adb_command_vm(f"shell pm clear {p1}", indexNumber) #mono
     memuc.send_adb_command_vm(f"shell pm clear {p2}", indexNumber) #ps
@@ -76,7 +88,7 @@ def start_automate(indexNumber: int, p1: str, p2: str, p3: str):
     sleep(2)
     memuc.send_adb_command_vm(f"shell rm -r /data/data/{p1}/*", indexNumber) #ps
     sleep(5)
-    memuc.send_adb_command_vm("shell am start -a android.intent.action.VIEW -d https://s.scope.ly/hVasLShVL20", indexNumber)
+    memuc.send_adb_command_vm(f"shell am start -a android.intent.action.VIEW -d {invitationLink}", indexNumber)
     sleep(monoInterval)
     memuc.send_adb_command_vm(f"shell am force-stop {p1}", indexNumber) #ps
     memuc.send_adb_command_vm(f"shell am force-stop {p3}", indexNumber) #mono
